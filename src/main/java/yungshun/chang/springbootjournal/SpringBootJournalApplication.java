46285c793e1110783f8accf8b987685c3c315fdb
yungshun317 <seraph317@gmail.com>
2021-07-25 15:19:35 +0800

    [CHORE] Initialize a Spring Boot project

---
+++ src/main/java/yungshun/chang/springbootjournal/SpringBootJournalApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springbootjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJournalApplication.class, args);
+    }
+
+}
